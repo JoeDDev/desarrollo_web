a0c7ce68c4e2244bf4395d64b2a463917891c7aa
163625 - JOSE DAVID ROJAS FUENTES <jrojasf2@miumg.edu.gt>
2019-08-15 21:49:38 -0600

    se procediò a la creaciòn del carrito y poder cargarle las imàgenes, aun falta...

--- main_online.js
+++ main_online.js
@@ -8,43 +8,46 @@ let draggedItem = null;
 
 for (let i = 0; i < list_items.length; i++) {
 	const item = list_items[i];
-
+	 
 	item.addEventListener('dragstart', function () {
 		draggedItem = item;
 		setTimeout(function () {
-            item.style.display = 'none';
-            variable = document.getElementsByTagName('div')[0].id;
-            console.log(variable);
+			item.style.display = 'block';            
 		}, 0)
 	});
 
 	item.addEventListener('dragend', function () {
 		setTimeout(function () {
-             draggedItem.style.display = 'block';
-             /* draggedItem = null; */
+             item.style.display = 'block';
+			 draggedItem = null;
+			 console.log(item.id);
 		},0);
 	})
 
+	
+
 	for (let j = 0; j < lists.length; j ++) {
 		const list = lists[j];
-        
+		        
 		list.addEventListener('dragover', function (e) {
 			e.preventDefault();
 		});
-		
+	
 		list.addEventListener('dragenter', function (e) {
 			e.preventDefault();
-			this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+			this.style.backgroundColor = 'white';
+			
 		});
 
 		list.addEventListener('dragleave', function (e) {
-			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+			this.style.backgroundColor = 'white';
+			
 		});
 
 		list.addEventListener('drop', function (e) {
 			console.log('drop');
 			this.append(draggedItem);
-			this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+			this.style.backgroundColor = 'white';
 		});
 	} 
 }
